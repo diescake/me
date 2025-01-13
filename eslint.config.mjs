0a7bba442751e2b8bc57ffe6aacdc49e2bab2f54
diescake <gachapining@gmail.com>
2025-01-13 20:36:38 +0900

    update eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,19 @@
 import nextPlugin from '@next/eslint-plugin-next'
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'dist/**'],
   },
-  ...compat.extends(
-    'plugin:@next/next/recommended',
-    'plugin:@next/next/core-web-vitals'
-  ),
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
-      '@typescript-eslint': (await import('@typescript-eslint/eslint-plugin'))
-        .default,
+      '@typescript-eslint': typescriptPlugin,
     },
     languageOptions: {
-      parser: (await import('@typescript-eslint/parser')).default,
+      parser: typescriptParser,
       parserOptions: {
         project: './tsconfig.json',
       },
@@ -46,6 +33,8 @@ export default [
     rules: {
       'prefer-const': 'error',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
+      ...nextPlugin.configs['recommended'].rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
     },
   },
 ]
